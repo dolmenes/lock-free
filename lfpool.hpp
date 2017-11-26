192e889d0eaa556401b6af3a62b2b107c5372321
Trauma <calambre75@gmail.com>
2017-11-26 10:11:30 +0100

    Update lfpool.hpp

--- lfpool.hpp
+++ lfpool.hpp
@@ -4,66 +4,101 @@
 #include <atomic>
 #include <cassert>
 #include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <type_traits>
 
 namespace lfs {
 
 template< typename T > class lfPool {
-  struct Node {
-    std::atomic_flag busy = ATOMIC_FLAG_INIT;
+  struct Slot {
+    std::atomic_flag busy;
     union {
       T data;
-      char dummy[sizeof( T )];
+      char dummy;
     };
 
-    ~Node( ) { }
-    Node( ) = default;
-    Node( Node && ) = default;
+    ~Slot( ) { }
+    Slot( ) { }
   };
 
   struct Block {
-    Block *next = nullptr;
-    Node nodes[1];
+    Block *next;
+    Slot slots[1];
 
     ~Block( ) { }
-    Block( ) = default;
-    Block( Block && ) = default;
+    Block( ) { }
   };
 
-  std::atomic< Block * > block = nullptr;
-  std::atomic_flag resizing = ATOMIC_FLAG_INIT;
+  struct EmptyBlock { EmptyBlock *next; };
+
+  volatile std::atomic< Block * > block;
+  std::atomic_flag resizing;
 #ifndef NDEBUG
   // Genera un error si intentamos hacer algo durante el destructor.
-  std::atomic_flag destroy = ATOMIC_FLAG_INIT;
+  // true -> Ok.
+  // false -> Estamos en el destructor.
+  std::atomic_flag destroy;
 #endif
-  size_t npb;
+  size_t slotsCount;
 
   Block *newBlock( ) noexcept( false ) {
     assert( destroy.test_and_set( ) );
 
-    Block *blk = reinterpret_cast< Block * >( new char[sizeof( Block ) + ( sizeof( Node ) * ( npb - 1 ) )] );
-    for( size_t idx = 0; idx < npb; ++idx ) {
-      new ( &(blk->nodes[idx]) ) Node( );
+    size_t alloc = sizeof( Block ) + ( sizeof( Slot ) * ( slotsCount - 1 ) );
+    std::cout << "newBlock( ). Allocate " << alloc << " bytes.\n";
+
+    Block *blk = reinterpret_cast< Block * >( new char[alloc] );
+
+    // Inicializamos los slots.
+    for( size_t idx = 0; idx < slotsCount; ++idx ) {
+      blk->slots[idx].busy.clear( std::memory_order_release );
     }
-  
+
     return blk;
   }
+
 public:
   static constexpr bool is_dynamic_resizable = true;
 
-  ~lfPool( ) noexcept {
+  ~lfPool( ) noexcept ( std::is_nothrow_destructible< T >::value ) {
 #ifndef NDEBUG
     destroy.clear( );
 #endif
     Block *curr = block;
-    while( block ) {
+
+    while( curr ) {
+      // Si el destructor no es trivial, lo invocamos.
+      if( !std::is_trivially_destructible< T >::value ) {
+        for( size_t idx = 0; idx < slotsCount; ++idx ) {
+          if( curr->slots[idx].busy.test_and_set( ) ) {
+            curr->slots[idx].data.~T( );
+          }
+        }
+      }
+
       Block *tmp = curr->next;
-      delete[] block;
-      block = tmp;
+      delete curr;
+      curr = tmp;
     }
   }
   lfPool( ) = delete;
-  explicit lfPool< T >( size_t n ) noexcept( false ) : npb( n ) {
-    block = newBlock( );
+  explicit lfPool( size_t n ) noexcept( false ) :
+      block( nullptr ),
+      resizing( ATOMIC_FLAG_INIT ),
+#ifndef NDEBUG
+      destroy( ATOMIC_FLAG_INIT ),
+#endif
+      slotsCount( n )
+  {
+    assert( n );
+
+#ifndef NDEBUG
+    destroy.test_and_set( ); // NO estamos en el destructor.
+#endif
+    Block *tmp = newBlock( );
+    tmp->next = nullptr;
+    block = tmp;
   }
   lfPool( const lfPool & ) = delete;
   lfPool( lfPool &&o ) = delete;
@@ -78,27 +113,19 @@ public:
 
       // Recorremos todos los bloques y todos los nodos, buscando 1 libre.
       while( curr ) {
-        for( size_t idx = 0; idx < npb; ++idx ) {
-          if( !( curr->nodes[idx].busy.test_and_set( ) ) ) {
-            return &curr->nodes[idx].data;
+        for( size_t idx = 0; idx < slotsCount; ++idx ) {
+          if( !( curr->slots[idx].busy.test_and_set( std::memory_order_acquire ) ) ) {
+            // Encontramos un slot libre.
+            return &( curr->slots[idx].data );
           }
         }
 
         curr = curr->next;
       }
 
-      if( !resize ) break;
-
-      // Creamos un nuevo Block.
-      if( !resizing.test_and_set( ) ) {
-        // Tenemos el cerrojo.
-        Block *blk = newBlock( );
-        blk->next = nullptr;
-        // Colocamos el bloque al principio de la lista.
-        block.store( blk );
-        // Quitamos el cerrojo.
-        resizing.clear( );
-      }
+      // Si llegamos aquí, es que no hay slots libres en ningún bloque.
+      std::cerr << "¡ No hay slots libres !\n";
+      abort( );
     }
 
     return nullptr;
@@ -106,10 +133,17 @@ public:
   void unget( T &data ) noexcept {
     assert( destroy.test_and_set( ) );
 
-    Node *n = reinterpret_cast< Node * >( reinterpret_cast< std::atomic_flag * >( &data ) - 1 );
+    Slot *s = reinterpret_cast< Slot * >( reinterpret_cast< std::atomic_flag * >( &data ) - 1 );
 
-    n.busy.clear( );
+    s->~T( );
+    s->busy.clear( );
   }
+
+  size_t tsize( ) const { return sizeof( T ); }
+  size_t ptrSize( ) const { return sizeof( Block * ); }
+  size_t emptyBlockSize( ) const { return sizeof( EmptyBlock ); }
+  size_t blockSize( ) const { return sizeof( Block ); }
+  size_t slotSize( ) const { return sizeof( Slot ); }
 };
 
 }
